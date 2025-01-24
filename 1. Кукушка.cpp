1b0958a2f6b31f24a4dbfbc74348c4551dad3564
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-01-24 22:07:05 +0300

    Add files via upload

---
+++ 1. Кукушка.cpp
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    system("chcp 1251"); // Установить кодировку
+    int hour;
+    std::cout << "Который час? Введите число до 12 включительно: ";
+    std::cin >> hour;
+
+    int count = 0;
+    while (count != hour) {
+        std::cout << "Ку-ку!\n";
+        ++count;
+    }
+}
+
