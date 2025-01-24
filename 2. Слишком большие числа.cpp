b1a31631218432820b77984e94287bb67a752e09
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-01-24 22:58:18 +0300

    Add files via upload

---
+++ 2. Слишком большие числа.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    system("chcp 1251"); // Установить кодировку
+    int number;
+    std::cout << "Введите число: ";
+    std::cin >> number;
+    //посчитать количество цифр в числе
+    int count = 0;
+
+    while (number != 0) {
+        number /= 10;
+        ++count;
+    }
+    
+    std::cout << "Количество цифр в числе: " << count;
+}
+
