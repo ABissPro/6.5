4d3851f4310ec85ea04c7e911af22a903826734a
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-01-25 00:08:58 +0300

    Add files via upload

---
+++ 4. Счастливый билетик.cpp
@@ -0,0 +1,42 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    system("chcp 1251"); // Установить кодировку
+    int number;
+    std::cout << "Введите номер билетика: ";
+    std::cin >> number;
+    int leftSum = 0;
+    int rightSum = 0;
+    int oneDigit;
+    int current;
+
+    
+    while (number != 0) {
+        oneDigit = number % 10;
+        rightSum += oneDigit;
+        number /= 10;
+        if (number < 1000) break;
+        current = number;
+    }
+    std::cout << "Сумма цифр справа: " << rightSum << "\n";
+   
+    while (number != 0) {
+        oneDigit = number % 10;
+        leftSum += oneDigit;
+        number /= 10;
+        current = number;
+    }
+    std::cout << "Сумма цифр слева: " << leftSum << "\n";
+
+    if (rightSum == leftSum) {
+        std::cout << "Билетик счастливый!!!";
+    }
+    else {
+        std::cout << "Повезёт в следующий раз!";
+    }
+}
+
+
